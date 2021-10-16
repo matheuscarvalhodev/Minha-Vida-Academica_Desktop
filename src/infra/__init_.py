7492ae991619fdc89de4210988b8fd97ec78ab58
Matheus Carvalho <math.k.carvalho@gmail.com>
2021-10-16 20:07:50 -0300

    Aplicando nova atualização

--- src/infra/__init_.py
+++
@@ -1,8 +0,0 @@
-from .conexao import (
-    login,
-    solicita_dados,
-    enviar_dados,
-    ponto,
-    verifica_vacinacao,
-    verifica_tipo,
-)
--- src/utilities/__init__.py
+++
@@ -1,2 +0,0 @@
-from .util import read_barcodes, dados_aluno, sleep
-from .detalhes_ui import detalhes, normal, displayImage
